a36d16076d098416fcd9699556a8a4ee4769a2cf
Miguel Marroyo <miguelmarroyob@gmail.com>
2017-04-22 14:15:53 +0200

    Añadida imagen

--- src/InterfazGrafica/InicioSesion.java
+++ src/InterfazGrafica/InicioSesion.java
@@ -3,7 +3,9 @@ package InterfazGrafica;
 import java.awt.BorderLayout;
 import java.awt.Container;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
@@ -28,6 +30,13 @@ public class InicioSesion extends JFrame{
 		JPanel panel = new PanelInicioSesion();
 		
 		contenedor.add(panel, BorderLayout.CENTER);
+		
+		ImageIcon image = new ImageIcon("eCourses.png");
+		JLabel label = new JLabel("", image, JLabel.CENTER);
+		JPanel panelImage = new JPanel(new BorderLayout());
+		panelImage.add(label, BorderLayout.CENTER );
+		
+		contenedor.add(panelImage, BorderLayout.NORTH);
 		
 		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		ventana.setSize(500, 500);
